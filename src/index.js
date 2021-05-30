f3c84e205633037f166f362db90fa8249d19884c
liwangping <liwangping@mail.hypers.com>
2021-05-30 15:38:15 +0800

    feat(静态资源): 增加webpack对于静态资源的加载，对于css之类的资源使用了内置的加载机制

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+import _ from "lodash";
+import "./style.css";
+import Icon from "./icon.png";
+import Data from "./data.xml";
+import Notes from "./data.csv";
+import TestData from "./test.json";
+
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  // add thw image to our existing div
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
+  console.log(TestData);
 
   return element;
 }
